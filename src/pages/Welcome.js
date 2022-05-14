b14cd88f00d730c92f6fab75fe75cd9ff59d45e0
eeyfesubalew <esubalew.seife@gmail.com>
2022-05-14 07:16:34 -0800

    dynamic route in components

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,4 +1,12 @@
+import { Route } from "react-router-dom";
 const Welcome = () => {
-  return <h1>This is the Welcome page</h1>;
+  return (
+    <section>
+      <h1>This is the Welcome page</h1>
+      <Route path="/welcome/new-user">
+        <p>Welcome new user</p>
+      </Route>
+    </section>
+  );
 };
 export default Welcome;
