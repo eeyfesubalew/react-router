805bbc21a7f48fbf6ebe064da17fcac2c58bac5a
eeyfesubalew <esubalew.seife@gmail.com>
2022-05-14 07:24:46 -0800

    redirecting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Products from "./pages/Products";
 import Welcome from "./pages/Welcome";
 import MainHeader from "./components/Mainheader";
@@ -10,6 +10,9 @@ function App() {
 
       <main>
         <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome"></Redirect>
+          </Route>
           <Route path="/welcome">
             <Welcome />
           </Route>
